846c4da1a80b288be1d5e8cf717bccedeb602c4e
Oh_Blow_Mayaya <maya881218@gmail.com>
2022-01-09 12:25:16 +0800

    Accounting

---
+++ Final_Web/Controllers/ListController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Final_Web;
+
+namespace Final_Web.Controllers
+{
+    public class ListController : Controller
+    {
+        private ToDoEntities db = new ToDoEntities();
+
+        // GET: List
+        public ActionResult Index()
+        {
+            return View(db.List.ToList());
+        }
+
+        // GET: List/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            List list = db.List.Find(id);
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View(list);
+        }
+
+        // GET: List/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: List/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SN,Title,Category,Money,Date")] List list)
+        {
+            if (ModelState.IsValid)
+            {
+                db.List.Add(list);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(list);
+        }
+
+        // GET: List/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            List list = db.List.Find(id);
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View(list);
+        }
+
+        // POST: List/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SN,Title,Category,Money,Date")] List list)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(list).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(list);
+        }
+
+        // GET: List/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            List list = db.List.Find(id);
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View(list);
+        }
+
+        // POST: List/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            List list = db.List.Find(id);
+            db.List.Remove(list);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
